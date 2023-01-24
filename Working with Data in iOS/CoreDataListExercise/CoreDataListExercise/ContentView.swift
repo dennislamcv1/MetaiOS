beaf702e021346b2bc4e7a8d35d9a919cb3c2d62
Dennis Lam <39684447+dennislamcv1@users.noreply.github.com>
2023-01-24 11:30:51 +0800

    Add files via upload

--- Working with Data in iOS/CoreDataListExercise/CoreDataListExercise/ContentView.swift
+++ Working with Data in iOS/CoreDataListExercise/CoreDataListExercise/ContentView.swift
@@ -4,8 +4,11 @@ import CoreData
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
     
-    var desserts = [PlaceHolderDessert]()
-        
+    @FetchRequest(
+            sortDescriptors: [],
+            animation: .default)
+    private var desserts: FetchedResults<Dessert>
+            
     var body: some View {
         NavigationView{
             VStack{
@@ -34,7 +37,57 @@ struct ContentView: View {
     
     
     
-    func createDesserts() { }
+    func createDesserts() {
+        let profiterole = Dessert(context: viewContext)
+        profiterole.name = "Profiterole"
+        profiterole.size = "Large"
+        profiterole.price = 2.99
+                
+        let cremeBurlee = Dessert(context: viewContext)
+        cremeBurlee.name = "Crème Burlée"
+        cremeBurlee.size = "Medium"
+        cremeBurlee.price = 3.99
+        
+        let iceCream = Dessert(context: viewContext)
+        iceCream.name = "Ice Cream"
+        iceCream.size = "Extra Large"
+        iceCream.price = 5.99
+        
+        let applePie = Dessert(context: viewContext)
+        applePie.name = "Apple Pie"
+        applePie.size = "Small"
+        applePie.price = 2.99
+        
+        let imperatrice = Dessert(context: viewContext)
+        imperatrice.name = "Riz à l'impératrice"
+        imperatrice.size = "Small"
+        imperatrice.price = 2.99
+        
+        let cheeseCake = Dessert(context: viewContext)
+        cheeseCake.name = "Cheese Cake"
+        cheeseCake.size = "Extra Small"
+        cheeseCake.price = 4.99
+        
+        let carrotCake = Dessert(context: viewContext)
+        carrotCake.name = "Carrot Cake"
+        carrotCake.size = "Standard"
+        carrotCake.price = 3.99
+        
+        let piece = Dessert(context: viewContext)
+        piece.name = "Pièce montée"
+        piece.size = "Medium"
+        piece.price = 5.99
+        
+        let eclair = Dessert(context: viewContext)
+        eclair.name = "Éclair"
+        eclair.size = "Large"
+        eclair.price = 3.99
+        
+        let crepe = Dessert(context: viewContext)
+        crepe.name = "Crêpe Suzette"
+        crepe.size = "Extra Large"
+        crepe.price = 3.99
+    }
     
 }
 
--- Working with Data in iOS/CoreDataListExercise/CoreDataListExercise/CoreDataListExerciseApp.swift
+++ Working with Data in iOS/CoreDataListExercise/CoreDataListExercise/CoreDataListExerciseApp.swift
@@ -16,8 +16,9 @@ struct CoreDataListExerciseApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
-        }
+                    ContentView()
+                        .environment(\.managedObjectContext,
+                                     persistenceController.container.viewContext)
+                }
     }
 }
