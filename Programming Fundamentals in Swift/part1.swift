badd1371e3ccc64d0412eccf613eb8062656b5c8
Dennis Lam <39684447+dennislamcv1@users.noreply.github.com>
2023-08-23 11:30:47 +0800

    Add files via upload

---
+++ Programming Fundamentals in Swift/part1.swift
@@ -0,0 +1,11 @@
+class VirtualBankSystem {
+    var accountType = ""
+    func welcomeCustomer() {
+        print("Welcome to your virtual bank system.")
+    }
+    func onboardCustomerAccountOpening() {
+        print("What kind of account would you like to open?")
+        print("1. Debit account")
+        print("2. Credit account")
+    }
+…} while virtualBankSystem.accountType == ""
---
+++ Programming Fundamentals in Swift/part2.swift
@@ -0,0 +1,15 @@
+struct BankAccount {
+    var debitBalance = 0
+    var creditBalance = 0
+    let creditLimit = 100
+    var debitBalanceInfo: String {
+        "Debit balance: $\(debitBalance)"
+    }
+    var availableCredit: Int {
+        creditLimit + creditBalance
+    }
+…bankAccount.creditWithdraw(101)
+bankAccount.creditWithdraw(100)
+bankAccount.creditDeposit(50)
+bankAccount.creditDeposit(50)
+bankAccount.creditDeposit(100)
